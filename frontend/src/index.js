aedc3886715c8603db09b26a32456904c6ba33ef
Navpreet Singh <navpreet.singh@biofourmis.com>
2025-03-04 22:09:14 +0530

    fixed body margin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
