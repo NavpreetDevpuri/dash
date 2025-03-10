975a1fb75d164ef2ca7cea3adf3e93088b24a4a9
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2025-03-10 05:37:33 +0530

    Added migration imports for various data sources

---
+++ backend/migrations/__init__.py
@@ -0,0 +1,7 @@
+from . import import_contacts
+from . import import_slack_messages
+from . import import_email_data
+from . import import_dineout_food_preferences
+from . import import_restaurant_data
+from . import import_calendar_data
+from . import import_notes_data 
